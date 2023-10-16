75c108f746e12993d14c43d834b001385f72b5cd
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-10-16 14:36:59 +0200

    Added some re-evaluating components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import "./App.css";
 import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
@@ -9,10 +10,11 @@ function App() {
   const toggleParagraphHandler = () => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
   };
+  console.log('App RUNNING');
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
