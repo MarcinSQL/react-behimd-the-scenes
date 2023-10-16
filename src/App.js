ffc302f8aab236efdae57f8f4b6a31a5e45e9241
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-10-16 18:14:08 +0200

    Added useCallback hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 import "./App.css";
 import Button from "./components/UI/Button/Button";
@@ -7,10 +7,12 @@ import DemoOutput from "./components/Demo/DemoOutput";
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
-  const toggleParagraphHandler = () => {
+  console.log("App RUNNING");
+
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
-  console.log('App RUNNING');
+  }, []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
